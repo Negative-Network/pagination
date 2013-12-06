3d90c96e5e20ee035592351fd12c4867ad51ff0b
Pierre Blarre <pierre@negative-network.com>
2013-12-06 15:27:54 +0100

    Changing 'All' to infinity symbol (&infin;)

--- paginator/views/paginator/pagination.php
+++ paginator/views/paginator/pagination.php
@@ -51,7 +51,7 @@ $last_string = '&nbsp;>>';
             echo '<a href="?amount=100" rel="last">100</a>';
         if ($items_per_page == 'all')
             echo __('All'); else
-            echo '<a href="?amount=0" rel="last">All</a>';
+            echo '<a href="?amount=0" rel="last">&infin;</a>';
         echo '</div>';
     }
     ?>
