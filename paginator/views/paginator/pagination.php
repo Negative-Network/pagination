58e4291ad25ef73f9fb01ee959d2c5dc55d78177
Pierre Blarre <pierre@negative-network.com>
2014-06-11 12:20:03 +0200

    source formatting

--- paginator/views/paginator/pagination.php
+++ paginator/views/paginator/pagination.php
@@ -3,6 +3,9 @@ $first_string = '<<&nbsp;';
 $previous_string = '<&nbsp';
 $next_string = '&nbsp;>';
 $last_string = '&nbsp;>>';
+
+if (!isset($current_controller)) $current_controller = strtolower(Request::current()->controller());
+$href = URL::base().Request::current()->uri();
 ?>
 <style type="text/css">
     <!--
@@ -19,39 +22,41 @@ $last_string = '&nbsp;>>';
 <div class="pagination right">
     <?php
     echo '<div class="right">';
-    if ($first) {
-        echo '<a href="' . $first . '" rel="first">' . $first_string . '</a>';
+    if ($first)
+    {
+        echo '<a href="'.$href.$first.'" rel="first" class="list_pagination" controller="'.$current_controller.'">'.$first_string.'</a>';
     }
-    if ($previous) {
-        echo '<a href="' . $previous . '" rel="previous">' . $previous_string . '</a>';
+    if ($previous)
+    {
+        echo '<a href="'.$href.$previous.'" rel="previous" class="list_pagination" controller="'.$current_controller.'">'.$previous_string.'</a>';
     }
-    foreach ($pages_in_range as $key => $value) {
-        $value ? print('<a href="' . $value . '">' . $key . '</a>')  : print $key;
+    foreach ($pages_in_range as $key => $value)
+    {
+        $value ? print('<a href="'.$href.$value.'" class="list_pagination" controller="'.$current_controller.'">'.$key.'</a>')  : print $key;
     }
-    if ($next) {
-        echo '<a href="' . $next . '" rel="next">' . $next_string . '</a>';
+    if ($next)
+    {
+        echo '<a href="'.$href.$next.'" rel="next" class="list_pagination" controller="'.$current_controller.'">'.$next_string.'</a>';
     }
-    if ($last) {
-        echo '<a href="' . $last . '" rel="last">' . $last_string . '</a>';
+    if ($last)
+    {
+        echo '<a href="'.$href.$last.'" rel="last" class="list_pagination" controller="'.$current_controller.'">'.$last_string.'</a>';
     }
     echo '</div>';
-    if ($items_per_page) {
+
+    if ($items_per_page)
+    {
         echo '<div class="right first">';
-        if ($items_per_page == 10)
-            echo $items_per_page; else
-            echo '<a href="?amount=10" rel="last">10</a>';
-        if ($items_per_page == 20)
-            echo $items_per_page; else
-            echo '<a href="?amount=20" rel="last">20</a>';
-        if ($items_per_page == 50)
-            echo $items_per_page; else
-            echo '<a href="?amount=50" rel="last">50</a>';
-        if ($items_per_page == 100)
-            echo $items_per_page; else
-            echo '<a href="?amount=100" rel="last">100</a>';
-        if ($items_per_page == 'all')
-            echo '&infin;'; else
-            echo '<a href="?amount=0" rel="last">&infin;</a>';
+        if ($items_per_page == 10) echo $items_per_page;
+        else echo '<a href="'.$href.'?amount=10" rel="last" class="list_pagination" controller="'.$current_controller.'">10</a>';
+        if ($items_per_page == 20) echo $items_per_page;
+        else echo '<a href="'.$href.'?amount=20" rel="last" class="list_pagination" controller="'.$current_controller.'">20</a>';
+        if ($items_per_page == 50) echo $items_per_page;
+        else echo '<a href="'.$href.'?amount=50" rel="last" class="list_pagination" controller="'.$current_controller.'">50</a>';
+        if ($items_per_page == 100) echo $items_per_page;
+        else echo '<a href="'.$href.'?amount=100" rel="last" class="list_pagination" controller="'.$current_controller.'">100</a>';
+        if ($items_per_page == 'all') echo '&infin;';
+        else echo '<a href="'.$href.'?amount=0" rel="last" class="list_pagination" controller="'.$current_controller.'">&infin;</a>';
         echo '</div>';
     }
     ?>
